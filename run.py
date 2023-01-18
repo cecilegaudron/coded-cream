302c32728d71f7ef41399e5f0f7b5f167e590a2a
cecilegaudron <ce.gaudron@gmail.com>
2023-01-18 15:52:33 +0000

    Display containers values from worksheet

--- run.py
+++ run.py
@@ -1,19 +1,37 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
+"""
+Declare creds, access to the google drive and excel document 
+Code from the Love Sandwiches project
+"""
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
 
+
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('coded-cream')
 
-cream = SHEET.worksheet('details')
 
-data = cream.get_all_values()
+ingredient = SHEET.worksheet('details')
 
-print(data)
+
+def container_choice():
+    """
+    Call the first column with the details and display choices for containers
+    Input the client's choice
+    """
+    print("What is the container ?")
+    container_details = ingredient.col_values(1)[1:] # get the container values
+    print(indcontainer_details)
+
+
+container_choice()
+
+
