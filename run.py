18547a9c47cb02eb02cf6cdb462ef944723795be
cecilegaudron <ce.gaudron@gmail.com>
2023-01-19 13:57:00 +0000

    Function for input name and validating data

--- run.py
+++ run.py
@@ -21,15 +21,13 @@ SHEET = GSPREAD_CLIENT.open('coded-cream')
 
 ingredient = SHEET.worksheet('details')
 
-
+""" CODE WILL SEE AFTER
 def container_choice():
-    """
-    Call the first column with the details and display choices for containers
-    """
+    #Call the first column with the details and display choices for containers
     print("What is the container ?")
     container_details = ingredient.col_values(1)[1:] # get the container values
     
-    #Assign a number to a cell value
+    # Assign a number to a cell value
     container_num = []
     for i in range(1, 4):
         container_num.append(i)
@@ -37,19 +35,64 @@ def container_choice():
     container_convert = dict(zip(container_num, container_details))
     print(container_convert)
 
-    # convert string to value
 
 container_choice()
 
 
 def container_selection():
-    """
-    Receive the selection of the container
-    Input the client's choice
-    """
+   # Receive the selection of the container
+    #Input the client's choice
     container_str = input("Enter the selection with a number between 1 and 3: ")
-    # add a value linked with my containers
+    # check and valid the input
     # save this choice in the spreadsheet
 
 container_selection()
+"""
 
+def enter_name():
+    """
+    Ask the client's name with an input
+    
+    If the entry is correct, the user can go to the next step
+    """
+    while True:
+        name_str = input("Enter the name of the client: \n")
+
+        if validate_name(name_str):
+            print("Thank you!")
+            print(f"Order of '{name_str} 'is in progress.")
+            break
+    
+    return name_str
+
+
+def validate_name(name_str):
+    """
+    If/Else function for validating name
+    If no valid characters are entered, or if name is too short or too long
+    The user must add again the name until the input is good
+    """
+    if not name_str.isalpha(): # check if all characters are letters
+        print(f"You filled in: '{name_str}'.\n This is not valid. \n")
+        print("Please enter only letters.")
+        return False
+    elif len(name_str) <= 1: # check if the entry is more than 1 character
+        print(f"You filled in: '{name_str}'. \n")
+        print("The name should be longer than 1 character.\nPlease try again.")
+        return False
+    elif len(name_str) >= 10: # check if the entry is less than 10 characters
+        print(f"You filled in: '{name_str}'. \n")
+        print("The name should be shorter than 10 character. \n")
+        print("Please try again.")
+        return False
+    else:
+        return True
+
+
+def my_functions():
+    """
+    Calls different functions
+    """
+    enter_name()
+
+my_functions()
