450071d2ca74f1349f7002e6b54ac8a8c4f78ad1
cecilegaudron <ce.gaudron@gmail.com>
2023-01-22 12:18:50 +0000

    Remove unnecessary spaces, add necessary spaces, etc. To have a uniformity in the code

--- run.py
+++ run.py
@@ -25,19 +25,18 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 def enter_name():
     """
     Ask the client's name with an input
-    
     If the entry is correct, the user can go to the next step
     """
     while True:
-        name_str = input("Enter the name of the client: \n")
+        name_str = input("\nEnter the name of the client: \n")
 
         if validate_name(name_str):
             print("Thank you!")
-            print(f"Order of '{name_str} 'is in progress.")
+            print(f"Order of '{name_str}' is in progress.\n")
             break
-    
+
     differents_flavors()
-    
+
     return name_str
 
 
@@ -47,15 +46,15 @@ def validate_name(name_str):
     If no valid characters are entered, or if name is too short or too long
     The user must add again the name until the input is good
     """
-    if not name_str.isalpha(): # check if all characters are letters
+    if not name_str.isalpha():  # Check if all characters are letters
         print(f"You filled in: '{name_str}'.\nThis is not valid. \n")
         print("Please enter only letters.")
         return False
-    elif len(name_str) <= 1: # check if the entry is more than 1 character
+    elif len(name_str) <= 1:  # Check if the entry is more than 1 character
         print(f"You filled in: '{name_str}'. \n")
         print("The name should be longer than 1 character.\nPlease try again.")
         return False
-    elif len(name_str) >= 10: # check if the entry is less than 10 characters
+    elif len(name_str) >= 10:  # check if the entry is less than 10 characters
         print(f"You filled in: '{name_str}'. \n")
         print("The name should be shorter than 10 characters. \n")
         print("Please try again.")
@@ -64,19 +63,21 @@ def validate_name(name_str):
         return True
 
 # Define list of values with differents flavors
-flavors = ["Belgium Chocolate", "Madagascar Vanilla", "Sicilian Pistachio", "Speculoos", 
-"Indian Mango", "Peanut Butter", "Very Cherry", "Passion fruit", "Piedmont hazelnut", 
-"Salted butter caramel", "Rosemary lemon", "Basil apple"]
+flavors = ["Belgium Chocolate", "Madagascar Vanilla", "Sicilian Pistachio", 
+ "Speculoos", "Indian Mango", "Peanut Butter", "Very Cherry", "Passion Fruit", 
+ "Piedmont Hazelnut", "Salted Butter Caramel", "Rosemary Lemon", "Basil Apple"]
+
 
 def differents_flavors():
     """
     Display list of flavors with names and index
     """
-    list_flavors = enumerate(flavors) # display the index number from list
+    print("\nPlease select the choice of flavor:\n")
+    list_flavors = enumerate(flavors)  # Display the index number from list
 
     for flavor in list_flavors:
         print(flavor)
-    
+
     choose_flavor()
 
 
@@ -86,17 +87,16 @@ def choose_flavor():
     If it is a number, convert into integer
     And call the validate data function
     """
-    print("Please select the choice of flavor:\n")
-    flavor_choice = input("Choose a number between 1 and 11:\n")
+    flavor_choice = input("Choose a number:\n")
 
     while True:
         try:
-            flavor_number = int(flavor_choice) # Convert input into integer
-            if type(flavor_number) == int: # Check if the value is an integer
+            flavor_number = int(flavor_choice)  # Convert input into integer
+            if type(flavor_number) == int:  # Check if the value is an integer
                 validate_flavor(flavor_number)
 
         except ValueError:
-            print("The choice is not a number. Pleaser enter the choice again.")
+            print("The choice is not a number. Please enter the choice again.\n")
             return False
 
         return flavor_number
@@ -110,7 +110,7 @@ def validate_flavor(flavor_number):
         print(f"\nThe choice is: '{flavors[flavor_number]}'.\n")
         suggestion_topping()
     else:
-        print("The choosen number is not on the list. Please try again.")
+        print("The choosen number is not on the list. Please try again.\n")
         return False
 
 
@@ -122,21 +122,26 @@ def suggestion_topping():
     If incorrect entry, try again
     """
     print("Add a topping ? \n")
-    want_topping = input("Enter 'yes' to add a topping and 'n' to continue the order \n")
+    want_topping = input("Enter 'yes' to add a topping and 'n' to continue the order.\n")
 
     while True:
         if want_topping == "y":
-            print("Choose the topping on the list")
-            differents_toppings() # Call function with topping list
+            print("Choose the topping on the list.\n")
+            differents_toppings()  # Call function with topping list
             break
         
         elif want_topping == "n":
-            print("No topping wanted, continue the order")
+            print("No topping wanted, continue the order.\n")
             # Call next function
             break
         else:
-            print("Incorrect entry, please enter 'y' or 'n'")
+            print("Incorrect entry, please enter 'y' or 'n'.\n")
             return False
+
+
+# Define list of values with differents toppings
+toppings = ["Chocolate chips", "Marshmallows", "Rainbow sprinkles", 
+ "Chantilly cream", "Caramel"]
 
 
 def differents_toppings():
@@ -144,12 +149,9 @@ def differents_toppings():
     Display list of differents toppings
     Enumerate name and index of toppings
     """
-    toppings = ["Chocolate chips", "Marshmallows", "Rainbow sprinkles", "Chantilly cream", 
-    "Caramel"]
-
     list_toppings = enumerate(toppings)
 
-    for topping in list_toppings:
+    for topping in list_toppings:  # Display toppings with names and index
         print(topping)
 
 
